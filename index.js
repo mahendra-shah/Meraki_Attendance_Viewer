f4dc995fe1c7173d7e4f77aa2efb45c4794e20a2
bhupend-deshmukh <bhupendradeshmukh2018@gmail.com>
2022-12-17 16:46:34 +0530

    show data in model

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-// window.addEventListener("message", (event) => {
-//   // console.log(event,'YES THIS EVENT WALI COLSOLE HAI...');
-//   console.log(event.data, "only data ko print kr rha hu...");
-//   if (event.origin != 'https://meet.google.com') return
-//   const data = JSON.parse(event.data)
-//   const localData = localStorage.getItem('Meraki_Attendance_Data')
-//   if(localData){
-//     localData.push(data)
-//     // console.log(localData,'11111111111111111111');
-//     localStorage.setItem("Meraki_Attendance_Data",JSON.stringify(localData))
-//   }else{
-//     localStorage.setItem("Meraki_Attendance_Data",JSON.stringify([data]))
-//   }
-// });
-
 window.addEventListener("message", (event) => {
   console.log(event.data, "999999999999999999999999999999999");
   if (event.origin != "https://meet.google.com") return;
   const data = JSON.parse(event.data);
-  // console.log(data);
   const localData = JSON.parse(localStorage.getItem("Meraki_Attendance_Data"));
-  console.log(localData)
   if (localData) {
     localData.push(data);
     localStorage.setItem("Meraki_Attendance_Data", JSON.stringify(localData));
-    console.log('yes this is woling....')
     window.location.reload();
   } else {
     localStorage.setItem("Meraki_Attendance_Data", JSON.stringify([data]));
@@ -31,25 +13,16 @@ window.addEventListener("message", (event) => {
   }
 });
 
-
-
-
-// console.log(localStorage.getItem("Meraki_Attendance_Data"));
-let urlArr = [];
-const imgUrls = urlArr.filter((ele, ind) => {
-  if (ind <= 6) return ele;
-});
 const localRecords = JSON.parse(localStorage.getItem("Meraki_Attendance_Data"));
 const rev_record = localRecords.reverse()
-// console.log(localRecords, "local--------");
 const tbody = document.getElementById("history");
 for (let record in rev_record) {
   console.log(localRecords[record].PROFILE_URLS, "%%%%%%%%%%%%%%%%%%%%%%%%%%%%555");
   tbody.innerHTML += `
   <tr>
-  <td>${localRecords[record].meeting_title}</td>
-  <td>${localRecords[record].meeting_time}</td>
-  <td>
+    <td>${localRecords[record].meeting_title}</td>
+    <td>${localRecords[record].meeting_time}</td>
+    <td>
   ${localRecords[record].meet_duration}
   </td>
   <td>
@@ -74,43 +47,50 @@ for (let record in rev_record) {
     </ul>
   </td>
   <td><span
-  id='${record}'
-  onclick="testing(${record})"  
-  class="veiw"
-  type="button"
-  data-bs-toggle="modal"
-  data-bs-target="#myModal"
->
-view
-</span></td>
-  <td><img src="images/remove.svg" class="icon-at" />  </td>
+    id='${record}'
+    onclick="testing(${record})"  
+    class="veiw"
+    type="button"
+    data-bs-toggle="modal"
+    data-bs-target="#myModal"
+  >
+    view
+  </span></td>
+    <td><img src="images/remove.svg" class="icon-at" />  </td>
 </tr>
               `;
 }
 
 function testing(rec){
-  // const localData = JSON.parse(localStorage.getItem("Meraki_Attendance_Data"));
   const recData = rev_record[rec]
-  console.log(recData);
   const pop = document.getElementById('tb')
-
-  for(let ind in recData.attendee_names){
-    console.log(recData.PROFILE_URLS[ind]);
-    console.log(recData.attendedDurationInSec);
-    // console.log(recData[ind]);
+  const parsedNames = JSON.parse(recData.attendee_names)
+  const parsedDuration = JSON.parse(recData.attendedDurationInSec)
+  for(let ind = 0; parsedNames.length > ind; ind++ ){
+    let new_time = String(parsedDuration[ind]/60).split(".")
+    let create_tmt = `${new_time[0]} : ${new_time[1].slice(0,2)}`
     pop.innerHTML += `
-        <tr>
-          <td class="col-xs-3"><img src="${recData.PROFILE_URLS[ind]}" class="icon-at" /></td>
-          <td class="col-xs-3">${recData.attendee_names[ind]}</td>
-          <td class="col-xs-6">${recData.attendedDurationInSec}</td>
+      <tr>
+          <td class="col-xs-3 profile-img"><img src="${recData.PROFILE_URLS[ind]}" class="img-fluid" /></td>
+          <td class="col-xs-5">${parsedNames[ind]}</td>
+          <td class="col-xs-4">${create_tmt}</td>
         </tr>
       `
   }
   
-  // recData.map((ele,ind)=>{
-  //   return (
-      
-  //   )
-  // })
 
+}
+
+
+function clearData(){
+  close = document.getElementById("tb")
+  close.innerHTML = `
+  <tr style="width: 480px !important;top:0px"
+  class="position-sticky bg-white w-auto"
+  >
+    <th class="col-4">First Name</th>
+    <th class="col-4">Last Name</th>
+    <th class="col-4">E-mail</th>
+  </tr>
+        `
 }
